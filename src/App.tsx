ebd7a1ff6e7ae381480c5ac50ab5fc1d4e82b50b
Hemant Chaudhary <ch.hemant33@gmail.com>
2024-08-15 20:02:10 +0530

    step 5

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 
 export const App = () => {
   const [inputString, setInputString] = useState("");
+  const [error, setError] = useState("");
   const [output, setOutput] = useState(0);
 
   const getIndex = (input) => {
@@ -17,21 +18,35 @@ export const App = () => {
     const delimiter = getDelimiter(inputString, newLinePos);
 
     const values = inputString.slice(newLinePos + 2).split(delimiter);
-    const output = values.reduce((a, b) => {
-      return a + Number(b);
-    }, 0);
+    try {
+      const output = values.reduce((a, b) => {
+        if (Number(b) < 0) {
+          throw new Error("Negative number not allowewd");
+        }
+        return a + Number(b);
+      }, 0);
+      setOutput(output);
+    } catch (e) {
+      const negativeValues = values.filter(value => Number(value) < 0);
+      const errorMsg = negativeValues.length > 1 ? negativeValues.join(',') : negativeValues[0]
+      setError("Negative numbers not allowewd " + errorMsg);
+    }
+  };
 
-    setOutput(output);
+  const textChanged = (value) => {
+    setInputString(value);
+    setError("");
   };
 
   return (
     <div>
       <input
         value={inputString}
-        onChange={(e) => setInputString(e.target.value)}
+        onChange={(e) => textChanged(e.target.value)}
       />
       <button onClick={calculate}>Calculate</button>
       <div>Output: {output}</div>
+      <div style={{ color: "red" }}>{error != "" ? error : null}</div>
     </div>
   );
 };
