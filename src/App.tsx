b1fee16ff9af093a8ff467c59cd8ff930796e72e
Hemant Chaudhary <ch.hemant33@gmail.com>
2024-08-15 19:23:58 +0530

    Step 1 and step 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import React, { useState } from "react";
 
-export const App = () => (
+export const App = () => {
+  const [inputString, setInputString] = useState("");
+  const [output, setOutput] = useState(0);
+
+  const calculate = () => {
+    const values = inputString.split(",");
+
+    const output = values.reduce((a, b) => {
+      return a + Number(b);
+    }, 0);
+    setOutput(output);
+  };
+
+  return (
     <div>
-      <h1 className="heading">This is React component</h1>
+      <input
+        value={inputString}
+        onChange={(e) => setInputString(e.target.value)}
+      />
+      <button onClick={calculate}>Calculate</button>
+      <div>Output: {output}</div>
     </div>
   );
+};
